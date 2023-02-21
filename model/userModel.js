c5e77e0a0964c90a7ee3e50291acccc20113293b
RameesRoshanck <ckmhdroshan@gmail.com>
2023-02-21 18:02:15 +0530

    initial commit

--- model/userModel.js
+++ model/userModel.js
@@ -1,1 +1,10 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    Name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("user", userSchema);
