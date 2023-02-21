a71532006d1c42c820697977fc7a097947303f51
BODHEESH <vcbodheesh@gmail.com>
2023-02-21 18:27:12 +0530

    login controller updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,19 @@
-export const login = (req,res,next)=>{
+export const login = async (req,res,next)=>{
+    try {
+        const {email, password}= req.body;
+        const isExist = await User.findOne({email:email})
+        if(isExist) throw res.status(409).json({msg:"user already exists"})
+    } catch (error) {
+        
+    }
+}
+
+export const register = (req, res, next )=>{
+   
     try {
         
     } catch (error) {
         
     }
+      
 }
